08367db500b4d25bd2510606680ca2297f649dea
Zoe Dj <studio3fun@gmail.com>
2019-07-28 17:11:54 -0400

    liri app

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
 # liri-node-app
+
+LIRI is a Language Interpretation and Recognition Interface. LIRI is a command line node app that takes in parameters and gives you back data.
+
+LIRI searchs Bands in Town for concerts :guitar: , Spotify for songs :notes: , and OMDB for movies :movie_camera: .
+
+LIRI can take in one of the four commands:
+
+1. `node liri.js concert-this <artist/band name here>`
+This will search the Bands in Town Artist Events API for an artist and render the following information about each event to the terminal:
+
+- Name of the venue
+- Venue location
+- Date of the Event
+
+2. `node liri.js spotify-this-song '<song name here>'`
+This will show the following information about the song in your terminal/bash window
+
+- Artist(s)
+- The song's name
+- A preview link of the song from Spotify
+- The album that the song is from
+
+If no song is provided then LIRI will default to "The Sign" by Ace of Base.
+
+
+3. `node liri.js movie-this '<movie name here>'`
+This will output the following information to your terminal/bash window:
+
+- Title of the movie.
+- Year the movie came out.
+- IMDB Rating of the movie.
+- Rotten Tomatoes Rating of the movie.
+- Country where the movie was produced.
+- Language of the movie.
+- Plot of the movie.
+- Actors in the movie.
+
+If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
+
+4. `node liri.js do-what-it-says`
+LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
+
+
+  * * note: If you want to clone this app from your github and run it yourself, you would need to supply your own `.env` file for it to work. You would need to replace the values with your API keys:
+  SPOTIFY_ID=your-spotify-id
+  SPOTIFY_SECRET=your-spotify-secret
+
+
+
+
+Include screenshots, gifs or videos of the app functioning
+Contain a link to a deployed version of the app
+Clearly list the technologies used in the app
+State your role in the app development
---
+++ liri.js
@@ -0,0 +1,175 @@
+require("dotenv").config();
+
+//create variable to access key.js file 
+var keys = require("./keys.js");
+//create variables to access nodeJS packages
+var Spotify = require("node-spotify-api");
+var fs = require("fs"); 
+var axios = require("axios");
+var moment = require ("moment");
+var spotify = new Spotify(keys.spotify);
+
+// The first argument is command 
+var command = process.argv[2];
+//search can contain more than one word
+var search = process.argv.slice(3).join(" ");
+
+
+function searchConcert(artist) {
+    var artist = search;
+    var concertQueryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+
+axios.get(concertQueryURL).then(
+    function(response) {
+        // console.log(response);
+        console.log("------- " + artist + " will perform at the folowing event -------");
+        console.log("Name of the venue: " + response.data[0].venue.name);
+        console.log("Venue location: " + response.data[0].venue.city);
+        console.log("Date of the event: " + moment(response.data[0].venue.datetime).format("MM/DD/YYYY")); //puts the date in the right format
+
+        // log to the txt.log
+        var concertLog = "-------" + artist + " will perform at the folowing event -------" + " \r " +
+        "Name of the venue: " + response.data[0].venue.name + "\r  "
+        "Venue location: " + response.data[0].venue.city + "\r  "
+        "Date of the event: " + moment(response.data[0].venue.datetime).format("MM/DD/YYYY") + "\r "  ;
+
+        //append text to log.txt
+        fs.appendFile("log.txt", concertLog, function (err){
+            if (err){
+                console.log(err);
+            }
+        });
+    })  
+};
+// searchConcert();
+
+function searchSong(song){
+    var song = search;
+    console.log("spotify key: " + spotify);
+
+    if (!song){
+        song= "The Sing";
+    };
+
+    spotify.search({ 
+        type: "track",
+        query: song
+    }, function(err, data){
+        if (err) {
+            return console.log("Error occurred: " + err);
+        }
+        //console.log(data.tracks.items[0]);
+        console.log("------- Spotify's information about song titled" + "'" + song + "'" + " ------- ");
+        console.log("Artist: " + data.tracks.items[0].album.artists[0].name);
+        console.log("Song's name: " + data.tracks.items[0].name);
+        console.log("Song's link: " + data.tracks.items[0].href);
+        console.log("Album: " + data.tracks.items[0].album.name);
+
+        //log to the log.txt
+        var songLog = "------- Spotify's information about song titled  " + "'" + song + "'" + " -------  "+ "\r  " +
+        "Artist: " + data.tracks.items[0].album.artists[0].name + "\r  " +
+        "Song's name: " + data.tracks.items[0].name + "\r  " +
+        "Song's link: " + data.tracks.items[0].href + " \r " +
+        "Album: " + data.tracks.items[0].album.name + "\r " ;
+
+        //append text to log.txt
+        fs.appendFile('log.txt', songLog, function (err){
+            if (err){
+                console.log(err);
+            }
+        });  
+    })
+};
+// searchSong();
+
+function searchMovie(movie){
+var movie = search;
+var movieQueryURL = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+
+if (!movie){
+    movie = "Mr. Nobody";
+};
+
+axios.get(movieQueryURL).then(
+    function(response) {
+    // console.log(response);
+    console.log("------------ Information about " + movie + " ------------")
+            console.log("Title: " + response.data.Title);
+            console.log("Year: " + response.data.Year);
+            console.log("IMDB Rating: " + response.data.imdbRating);
+            console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+            console.log("Country: " + response.data.Country);
+            console.log("Language: " + response.data.Language);
+            console.log("Plot: " + response.data.Plot);
+            console.log("Actors: " + response.data.Actors);
+
+
+    movieLog = "------------ Information about " + movie + " ------------" + "\r " +
+    "Title: " + response.data.Title + " \r" +
+    "Year: " + response.data.Year + "\r " + 
+    "IMDB Rating: " + response.data.imdbRating + "\r " +
+    "Rotten Tomatoes Rating: " + response.data.Ratings[1].Value + "\r " +
+    "Country: " + response.data.Country + "\r " +
+    "Language: " + response.data.Language + "\r " +
+    "Plot: " + response.data.Plot + "\r " +
+    "Actors: " + response.data.Actors + "\r "  ;
+
+    fs.appendFile("log.txt", movieLog, function (err){
+        if (err){
+            console.log(err);
+        }
+    });
+    })
+    .catch(function(error) {
+        if (error.response) {
+          console.log(error.response.data);
+          console.log(error.response.status);
+        }
+    })
+};
+// searchMovie();
+
+function doWhatItSays(){
+    // reads the content inside random.txt
+    fs.readFile("random.txt", "utf8", function (err, data){
+        if (err){
+            return console.log(err);
+        }
+        else{
+            // console.log(data);
+            // stores the returned data as an array
+            var random = data.split(",");
+            command = random[0];
+            search = random[1];
+            
+            liriApp(command, search);
+        }
+    });
+    
+};
+// doWhatItSays();
+
+function liriApp(command, search){
+    // The switch-case will direct which function gets run.
+    switch (command) {
+        case "concert-this":
+            searchConcert(search);
+            break;
+    
+        case "spotify-this-song":
+            searchSong(search);
+            break;
+        
+        case "movie-this":
+            searchMovie(search);
+            break;
+        
+        case "do-what-it-says":
+            doWhatItSays(search);
+            break;
+            //if the command is not provided, return a default message
+        default:
+            console.log("Please enter the command.")
+    }
+    };
+    liriApp(command, search);
